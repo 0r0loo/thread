65e7127afc6c6a7683383a85f7bedc5a8ef1a3fb
chotaegyu <chotg5592@gmail.com>
2022-07-31 21:36:35 +0900

    Add : 바뀌는 화면 세팅

--- screens/RootStack.js
+++ screens/RootStack.js
@@ -1,9 +1,25 @@
+import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import MainScreen from './MainScreen';
+import WriteScreen from './WriteScreen';
 
 const Stack = createNativeStackNavigator();
 
 function RootStack() {
-  return <Stack.Navigator></Stack.Navigator>;
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name={'Main'}
+        component={MainScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name={'Write'}
+        component={WriteScreen}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
 }
 
 export default RootStack;
