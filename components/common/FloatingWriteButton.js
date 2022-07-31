2098de3477d831b3151a2065adfa7341186841c8
chotaegyu <chotg5592@gmail.com>
2022-07-31 21:34:46 +0900

    Add : 글쓰기 버튼추가

---
+++ components/common/FloatingWriteButton.js
@@ -0,0 +1,76 @@
+import React, {useEffect, useRef} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {Animated, Platform, Pressable, StyleSheet, Text} from 'react-native';
+
+function FloatingWriteButton({hidden}) {
+  const navigation = useNavigation();
+
+  const onPress = () => {
+    console.log('플로팅버튼 눌림');
+    navigation.navigate('Write');
+  };
+
+  const animation = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.spring(animation, {
+      toValue: hidden ? 1 : 0,
+      useNativeDriver: true,
+      tension: 45,
+      friction: 5,
+    }).start();
+  }, [animation, hidden]);
+
+  return (
+    <Animated.View
+      style={[
+        styles.wrapper,
+        {
+          transform: [
+            {
+              translateY: animation.interpolate({
+                inputRange: [0, 1],
+                outputRange: [0, 88],
+              }),
+            },
+          ],
+          opacity: animation.interpolate({
+            inputRange: [0, 1],
+            outputRange: [1, 0],
+          }),
+        },
+      ]}>
+      <Pressable
+        style={({pressed}) => [
+          styles.button,
+          Platform.OS === 'ios' && {opacity: pressed ? 0.6 : 1},
+        ]}
+        onPress={onPress}>
+        <Text>+</Text>
+      </Pressable>
+    </Animated.View>
+  );
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    position: 'absolute',
+    bottom: 16,
+    right: 16,
+    width: 56,
+    borderRadius: 28,
+    shadowColor: '#4d4d4d',
+    shadowOffset: {width: 0, height: 4},
+    shadowRadius: 4,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+    backgroundColor: '#F9851B',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default FloatingWriteButton;
