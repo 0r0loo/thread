f928e520b2defdfb1f1b0416901132d55111a336
chotaegyu <chotg5592@gmail.com>
2022-07-31 23:40:01 +0900

    Modify : 메인스크린 작업중..

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import MainHeader from '../components/main/MainHeader';
-import {Text} from 'react-native';
 import Empty from '../components/main/Empty';
+import FloatingWriteButton from '../components/common/FloatingWriteButton';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 function MainScreen() {
   return (
-    <Container>
-      <MainHeader />
-      <Empty />
-    </Container>
+    <SafeAreaView edges={['bottom']} style={{flex: 1}}>
+      <Container>
+        <MainHeader />
+        <Empty />
+        <FloatingWriteButton />
+      </Container>
+    </SafeAreaView>
   );
 }
 const Container = styled.View`
