d88ecd4ca6d749b9349a1afa7e62cf2a5f1557b7
Blue Cho <blue@socialclub.co.kr>
2022-08-22 16:58:42 +0900

    Add : themeContext 추가

---
+++ src/hooks/useThemeContext.js
@@ -0,0 +1,9 @@
+import React, {useContext} from 'react';
+import {ThemeContext} from 'styled-components';
+
+const useThemeContext = () => {
+  const themeContext = useContext(ThemeContext);
+  return themeContext;
+};
+
+export default useThemeContext;
